65499aaa8a03777e9e0937fdb7a787e61b30bdfd
Komandr33 <and.kom2017@gmail.com>
2024-03-12 22:57:19 +0300

    Added styles in NavLink width in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,38 @@ import styles from './components/Site.module.css';
 import {PageOne} from './components/pages/PageOne';
 import {PageTwo} from './components/pages/PageTwo';
 import {PageThree} from './components/pages/PageThree';
-import {Navigate, Route, Routes} from 'react-router-dom';
+import {Navigate, NavLink, Route, Routes} from 'react-router-dom';
 import {Error404} from './components/pages/Error404';
 
 
 function App() {
+
+  const PATH = {
+    PAGE1: '/page1',
+    PAGE2: '/page2',
+    PAGE3: '/page3',
+  } as const
+
   return (
     <div>
       <div className={styles.header}><h1>HEADER</h1></div>
       <div className={styles.body}>
         <div className={styles.nav}>
-          Здесь будет навигация
+          <div><NavLink to={PATH.PAGE1}
+                        className={({isActive}) => isActive ? styles.active : styles.customLink}>Page1</NavLink></div>
+          <div><NavLink to={PATH.PAGE2}
+                        className={({isActive}) => isActive ? styles.active : styles.customLink}>Page2</NavLink></div>
+          <div><NavLink to={PATH.PAGE3}
+                        className={({isActive}) => isActive ? styles.active : styles.customLink}>Page3</NavLink></div>
         </div>
         <div className={styles.content}>
           <Routes>
-            <Route path={'/'} element={<Navigate to={'/page1'}/>}/>
-            <Route path={'/page1'} element={<PageOne/>}/>
-            <Route path={'/page2'} element={<PageTwo/>}/>
-            <Route path={'/page3'} element={<PageThree/>}/>
-            <Route path={'/error404'} element={<Error404/>}/>
-            <Route path={'/*'} element={<Navigate to={'/error404'}/>}/>
+            <Route path={'/'} element={<Navigate to={PATH.PAGE1}/>}/>
+            <Route path={PATH.PAGE1} element={<PageOne/>}/>
+            <Route path={PATH.PAGE2} element={<PageTwo/>}/>
+            <Route path={PATH.PAGE3} element={<PageThree/>}/>
+            <Route path={'*'} element={<Error404/>}/>
           </Routes>
-          {/*Здесь будут кроссовки*/}
         </div>
       </div>
       <div className={styles.footer}>abibas 2023</div>
