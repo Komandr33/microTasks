e4b1df608d78506092febf5d2314444d92cf8026
Komandr33 <and.kom2017@gmail.com>
2024-03-18 23:08:54 +0300

    added ProtectedPage.tsx and ProtectedRoute.tsx

---
+++ src/components/pages/ProtectedPage.tsx
@@ -0,0 +1,12 @@
+// @flow
+import * as React from 'react';
+
+type Props = {};
+export const ProtectedPage = (props: Props) => {
+  return (
+    <div>
+      <h3>Protected Page</h3>
+      <p>information for the admin</p>
+    </div>
+  );
+};
---
+++ src/routes/ProtectedRoute.tsx
@@ -0,0 +1,15 @@
+// @flow
+import * as React from 'react';
+import {ReactComponentElement} from 'react';
+import {Navigate} from 'react-router-dom';
+import {PATH} from './router';
+
+type PropsType = {
+  children: ReactComponentElement<any> // типизация для компонент React
+};
+export const ProtectedRoute: React.FC<PropsType> = ({children}) => {
+  const logged = false
+  return (
+    logged ? children : <Navigate to={PATH.ERROR}/>
+  );
+};
